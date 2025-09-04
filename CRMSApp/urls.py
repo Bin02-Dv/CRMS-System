3b4f843f67c7392a9fc56713be6459fb4c254f76
Bin02-Dv <alaminbinahmad@gmail.com>
2025-09-04 11:25:27 -0700

    9/4/2025 - 11:25pm

--- CRMSApp/urls.py
+++ CRMSApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("dash/", views.dash, name="dash"),
-    path("login/", views.login, name="login")
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout")
 ]
--- CRMSApp/views.py
+++ CRMSApp/views.py
@@ -1,9 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import models
+from django.contrib import auth
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def logout(request):
+    auth.logout(request)
+    return redirect("/login/")
+
+@login_required(login_url='/login/')
 def dash(request):
-    return render(request, "dash/dash.html")
+    current_user = request.user
+    all_users = models.AuthModel.objects.all()
+    context = {
+        "current_user": current_user,
+        "all_users": all_users
+    }
+    return render(request, "dash/dash.html", context)
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get("email", "")
+        password = request.POST.get("password", "")
+        
+        user = models.AuthModel.objects.filter(email=email).first()
+        
+        if user is not None:
+            if user.check_password(password):
+                auth.login(request, user)
+                return JsonResponse({
+                    "message": "Login successfully...",
+                    "success": True
+                })
+            else:
+                return JsonResponse({
+                    "message": "Incorrect Password!!",
+                    "success": False
+                })
+        else:
+            return JsonResponse({
+                "message": "Invalid Email!!",
+                "success": False
+            })
     return render(request, "dash/login.html")
