5c970294094ddf82c6b79898600fea1e3b6f7b54
Bin02-Dv <alaminbinahmad@gmail.com>
2025-09-17 18:45:26 -0700

    9/17/2025 - 6:45pm

--- CRMSApp/urls.py
+++ CRMSApp/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path("dash/", views.dash, name="dash"),
     path("login/", views.login, name="login"),
-    path("logout/", views.logout, name="logout")
+    path("logout/", views.logout, name="logout"),
+    path("user-management/", views.user_management, name="user-management"),
+    path("loan-management/", views.loan_management, name="loan-management"),
+    path("customer-registration/", views.customer_registration, name="customer-registration"),
+    path("customer-communication/", views.customer_communication, name="customer-communication"),
+    path("report/", views.report, name="report"),
 ]
--- CRMSApp/views.py
+++ CRMSApp/views.py
@@ -13,12 +13,79 @@ def logout(request):
 @login_required(login_url='/login/')
 def dash(request):
     current_user = request.user
+    context = {
+        "current_user": current_user
+    }
+    return render(request, "dash/dash.html", context)
+
+@login_required(login_url='/login/')
+def customer_registration(request):
+    current_user = request.user
+    context = {
+        "current_user": current_user
+    }
+    return render(request, "dash/customer-registration.html", context)
+
+@login_required(login_url='/login/')
+def customer_communication(request):
+    current_user = request.user
+    context = {
+        "current_user": current_user
+    }
+    return render(request, "dash/customer-communication.html", context)
+
+@login_required(login_url='/login/')
+def loan_management(request):
+    current_user = request.user
+    context = {
+        "current_user": current_user
+    }
+    return render(request, "dash/loan-management.html", context)
+
+@login_required(login_url='/login/')
+def user_management(request):
+    current_user = request.user
     all_users = models.AuthModel.objects.all()
+    
+    if request.method == 'POST':
+        full_name = request.POST.get("full_name", "")
+        email = request.POST.get("email", "")
+        phone_number = request.POST.get("phone_number", "")
+        role = request.POST.get("role", "")
+        password = request.POST.get("password", "")
+        confirm_password = request.POST.get("confirm_password", "")
+        
+        if models.AuthModel.objects.filter(email=email).exists():
+            return JsonResponse({
+                "message": f"Sorry this email {email} already exist!!",
+                "success": False
+            })
+        elif confirm_password != password:
+            return JsonResponse({
+                "message": "password and confirm password missed matched!!",
+                "success": False
+            })
+        else:
+            models.AuthModel.objects.create_user(
+                full_name=full_name, email=email, phone_number=phone_number, role=role, password=password, username="None"
+            )
+            return JsonResponse({
+                "message": "User Added successfully...",
+                "success": True
+            })
     context = {
         "current_user": current_user,
         "all_users": all_users
     }
-    return render(request, "dash/dash.html", context)
+    return render(request, "dash/user-management.html", context)
+
+@login_required(login_url='/login/')
+def report(request):
+    current_user = request.user
+    context = {
+        "current_user": current_user,
+    }
+    return render(request, "dash/report.html", context)
 
 def login(request):
     if request.method == 'POST':
