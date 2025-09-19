988d7e07b3842ecbead33ec0b405d999e24e190f
Bin02-Dv <alaminbinahmad@gmail.com>
2025-09-18 18:19:40 -0700

    8/18/2025 - 6:19pm

--- CRMSApp/models.py
+++ CRMSApp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 # Create your models here.
 
@@ -17,3 +19,30 @@ class AuthModel(AbstractUser):
     
     def __str__(self):
         return self.full_name
+
+class Customer(models.Model):
+    full_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=200, blank=True, unique=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    home_address = models.TextField(max_length=200, blank=True)
+    account_type = models.CharField(max_length=40, blank=True)
+
+
+class Loan(models.Model):
+    customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
+    loan_type = models.CharField(max_length=40, blank=True)
+    loan_amount = models.DecimalField(decimal_places=2, default=0.00, max_digits=10)
+    loan_period = models.IntegerField(null=True, default=0)
+    interest_rate = models.IntegerField(null=True, default=0)
+    date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=40, default='pending')
+    due_date = models.DateField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+
+        if self.loan_period and not self.due_date:
+            self.due_date = date.today() + relativedelta(months=int(self.loan_period))
+        super().save(*args, **kwargs)
+    
+    
+    
--- CRMSApp/views.py
+++ CRMSApp/views.py
@@ -21,8 +21,37 @@ def dash(request):
 @login_required(login_url='/login/')
 def customer_registration(request):
     current_user = request.user
+    
+    all_customers = models.Customer.objects.all()
+    
+    if request.method == 'POST':
+        full_name = request.POST.get("full_name")
+        email = request.POST.get("email")
+        phone_number = request.POST.get("phone_number")
+        home_address = request.POST.get("address")
+        account_type = request.POST.get("account_type")
+        
+        if not all([full_name, email, phone_number, home_address, account_type]):
+            return JsonResponse({
+                "message": "All Fields are required!!",
+                "success": False
+            })
+        elif models.AuthModel.objects.filter(email=email).exists():
+            return JsonResponse({
+                "message": f"Sorry this a customer with this email {email} already exist!!",
+                "success": False
+            })
+        else:
+            models.Customer.objects.create(
+                full_name=full_name, email=email, phone_number=phone_number, home_address=home_address, account_type=account_type
+            )
+            return JsonResponse({
+                "message": "Customer registration completed successfully...",
+                "success": True
+            })
     context = {
-        "current_user": current_user
+        "current_user": current_user,
+        "all_customers": all_customers
     }
     return render(request, "dash/customer-registration.html", context)
 
@@ -37,8 +66,42 @@ def customer_communication(request):
 @login_required(login_url='/login/')
 def loan_management(request):
     current_user = request.user
+    customers = models.Customer.objects.all()
+    loans = models.Loan.objects.all()
+    
+    if request.method == 'POST':
+        customer_id = request.POST.get("customer")
+        loan_type = request.POST.get("loan_type")
+        loan_amount = request.POST.get("loan_amount")
+        loan_period = request.POST.get("loan_period")
+        interest_rate = request.POST.get("interest_rate")
+        
+        if not all([customer_id, loan_type, loan_amount, loan_period, interest_rate]):
+            return JsonResponse({
+                "message": "All Fields are required!!",
+                "success": False
+            })
+        else:
+            try:
+                customer = models.Customer.objects.get(id=customer_id)
+            except models.Customer.DoesNotExist:
+                return JsonResponse({
+                    "message": f"Sorry we couldn't find any user with the ID - {customer_id}",
+                    "success": False
+                })
+            
+            models.Loan.objects.create(
+                customer=customer, loan_type=loan_type, loan_amount=loan_amount, loan_period=loan_period, interest_rate=interest_rate
+            )
+            return JsonResponse({
+                "message": f"Loan for the customer {customer.full_name} has been created successfully....",
+                "success": True
+            })
+        
     context = {
-        "current_user": current_user
+        "current_user": current_user,
+        "customers": customers,
+        "loans": loans
     }
     return render(request, "dash/loan-management.html", context)
 
